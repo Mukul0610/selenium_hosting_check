04f20d47307889b6f50bf3fe368265a41e96d7db
Mukul Rai <mukul93028@gmail.com>
2025-03-16 03:35:04 +0530

    first commit

--- app.py
+++ app.py
@@ -459,4 +459,4 @@ def search_challan():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=os.environ.get('DEBUG', 'False').lower() == 'true')
+    app.run(host='0.0.0.0', port=port)
--- start.sh
+++ start.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-gunicorn --bind 0.0.0.0:$PORT app:app
+# gunicorn --bind 0.0.0.0:$PORT app:app
+
+#!/bin/bash
+export PORT=${PORT:-8000}
+python app.py
